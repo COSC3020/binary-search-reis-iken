50a5c784e6814e68f5f21d8e479eff5a06c3e2a7
reis-iken <144738295+reis-iken@users.noreply.github.com>
2024-02-07 14:24:55 -0700

    code.js

--- code.js
+++ code.js
@@ -3,9 +3,9 @@ function binarySearch(list, element) {
     let right = list.length - 1;
     while (left <= right) {
         let middle = Math.floor((left + right) / 2);
-        if (arr[middle] === element) {
+        if (list[middle] === element) {
             return middle; }
-        else if (arr[middle] < element) {
+        else if (list[middle] < element) {
             left = middle + 1; }
         else if (arr[middle] > element) {
             right = middle - 1; }
