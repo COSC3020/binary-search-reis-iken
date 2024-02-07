2c0fb457e5b8adbaf2c0bc4b571f150f1f4d0a8f
reis-iken <144738295+reis-iken@users.noreply.github.com>
2024-02-07 14:22:19 -0700

    code.js
    Sources Cited: I made minimal use of ChatGPT to help with JavaScript syntax.

--- code.js
+++ code.js
@@ -1,3 +1,14 @@
 function binarySearch(list, element) {
+    let left = 0;
+    let right = list.length - 1;
+    while (left <= right) {
+        let middle = ((left + right) / 2);
+        if (arr[middle] === element) {
+            return middle; }
+        else if (arr[middle] < element) {
+            left = middle + 1; }
+        else if (arr[middle] > element) {
+            right = middle - 1; }
+    }
     return -1;
 }
